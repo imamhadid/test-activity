70e3f6840241eb5f198a29e44310e98f313c6dde
Imam Hadid <imamhadid792@gmail.com>
2022-09-12 18:40:24 +0800

    init: vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    getList() {
+      this.axios.get(api).then((response) => {
+        console.log(response.data)
+      })
+      // or
+      this.$http.get(api).then((response) => {
+        console.log(response.data)
+      })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
